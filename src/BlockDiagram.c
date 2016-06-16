271d76b2be4810ee0a0f470b8818d6cef23966ed
eemei <jjaanhorng@gmail.com>
2016-06-17 00:22:39 +0800

    Able to limit the icons to be displayed inside the buffer range.

--- src/BlockDiagram.c
+++ src/BlockDiagram.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "BlockDiagram.h"
 
-char buffer[50][50];
+char buffer[10][10];
 // char buffer1[1][1];
 
 const char alien1[][3] = {{" ^ "},
@@ -172,43 +172,49 @@ void bufferFiller(int row, int col, char symbol){
 
 void draw (char *image, int width, int length, int coorX, int coorY) {
 // void draw (char *image, int width, int length) {
-  int i, j, a, b, displayIconX, displayIconY, totalX, totalY, widthPrime, lengthPrime;
-  
-  bufferFiller(50, 50, 0x2b);
-  
-  totalX = coorX + width;
-  totalY = coorY + length;
-  
-  if ((totalX > 50)){
-    widthPrime = totalX - 50;
-    displayIconX = width - widthPrime;
-    lengthPrime = totalY - 50;
-    displayIconY = length - lengthPrime;
-    for (i = 0; i< displayIconX; i++){
-      for (j = 0; j< displayIconY; j++){
-        buffer[coorY+j][coorX+j] = *(image++);
-        printf("%c", buffer[coorY+i][coorX+j]);
+  int i , j, fixed_y, fixed_x;
+  if ((length+coorY)>10|| (width+coorX)>10){
+    if ((length+coorY)>10){
+      fixed_y = 11 - coorY;
+      printf("y= %d\n", fixed_y);
+    }
+    else {
+      fixed_y = length;
+      printf("*y =%d\n", fixed_y);
+    }
+    if ((width+coorX)>10){
+      fixed_x = 11 - coorX;
+      printf("x= %d\n", fixed_x);
+      for (i = 0; i < fixed_y; i++){
+        for (j = 0; j < fixed_x; j++){
+          buffer[coorX+i][coorY+j] = *(image++); 
+            printf("%c", buffer[coorX+i][coorY+j]);
+        }
+    printf("\n");
       }
-      printf("\n");
+    }
+    else {
+      fixed_x = width;
+      printf("*x = %d\n", fixed_x);
+      printf("$$$$$\n");
+      printf("%d  %d\n", fixed_x, fixed_y);
+      for (i = 0; i < fixed_y; i++){
+        for (j = 0; j < fixed_x; j++){
+          buffer[coorX+i][coorY+j] = *(image++); 
+            printf("%c", buffer[coorX+i][coorY+j]);
+        }
+    printf("\n");
+      }
     }
   }
-  else{
+  else {
+  
     for (i = 0; i < length; i++){
       for (j = 0; j < width; j++){
-        buffer[coorY+i][coorX+j] = *(image++); 
-        printf("%c", buffer[coorY+i][coorX+j]);
+        buffer[coorX+i][coorY+j] = *(image++); 
+        printf("%c", buffer[coorX+i][coorY+j]);
       }
       printf("\n");
     }
   }
-  
-  for (a=0; a<50; a++){
-    for (b=0; b<50; b++){
-      printf("%c", buffer[a][b]);
-    }
-    printf("\n");
-  }
-  
-  printf("\n\n");
 }
-
