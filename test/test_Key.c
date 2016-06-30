0c67be22de873e21dffdbe967eac9f47a6afd2c0
ee mei <lieweemei911@hotmail.com>
2016-06-30 19:57:48 +0800

    add shift left and right function

--- test/test_Key.c
+++ test/test_Key.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "Key.h"
+#include "BlockDiagram.h"
 
 void setUp(void){}
 
@@ -17,3 +18,12 @@ void test_fireBullet_return_1_when_spacebar_is_pressed(void){
   TEST_ASSERT_EQUAL(32, returnFire);
   TEST_IGNORE_MESSAGE("FIRE!");
 }
+
+void test_press_arrow_key_to_move_ship_left_or_right(void){
+/*   int hit;
+  hit = arrow();
+  TEST_ASSERT_EQUAL (75,hit); */
+// arrow();
+
+  continuPress() ;
+  }
