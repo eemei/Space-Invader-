0b60d9914c8b9a72289571f82796319fa2d825ce
Chu Jaan Horng <jjaanhorng@gmail.com>
2016-07-11 17:02:27 +0800

    keyboard FSM was separated from movementShipFSM. Tests for combination of these two FSM were done.

--- test/test_FSM.c
+++ test/test_FSM.c
@@ -8,8 +8,8 @@ void setUp(void){}
 
 void tearDown(void){}
 
-void test_ship_initialized_coordination_return_X_and_Y_twenty_four_and_forty_seven(void){
-  movementShip *pThis = initiateState();
+void test_ship_initialized_coordination_return_X_twenty_four_and_Y_forty_seven(void){
+  movementShip *pThis = initiateMovementState();
   
   pThis->state = START;
   movementShipFSM(pThis);
@@ -20,45 +20,52 @@ void test_ship_initialized_coordination_return_X_and_Y_twenty_four_and_forty_sev
 }
 
 void test_keyboard_no_press_should_return_the_direction_zero(void){
-  movementShip *pThis = initiateState();
+  movementShip *pThis = initiateMovementState();
   
   pThis->state = RELEASE;
+  pThis->keyboard->buttonState = BUTTONNOHIT;
   getKbPressed_ExpectAndReturn(pThis->kbPressed, BUTTON_RELEASED);
   movementShipFSM(pThis);
   
-  TEST_ASSERT_EQUAL(BUTTON_RELEASED, pThis->direction);
+  TEST_ASSERT_EQUAL(BUTTON_RELEASED, pThis->keyboard->direction);
+  TEST_ASSERT_EQUAL(BUTTONHIT, pThis->keyboard->buttonState);
   TEST_ASSERT_EQUAL(RELEASE, pThis->state);
 }
 
 void test_keyboard_press_left_should_return_the_direction_seventy_five(void){
-  movementShip *pThis = initiateState();
+  movementShip *pThis = initiateMovementState();
   
   pThis->state = RELEASE;
+  pThis->keyboard->buttonState = BUTTONHIT;
   getKbPressed_ExpectAndReturn(pThis->kbPressed, BUTTON_PRESSED);
   movementShipFSM(pThis);
   
-  TEST_ASSERT_EQUAL(KEY_LEFT, pThis->direction);
+  TEST_ASSERT_EQUAL(KEY_LEFT, pThis->keyboard->direction);
+  TEST_ASSERT_EQUAL(BUTTONHIT, pThis->keyboard->buttonState);
   TEST_ASSERT_EQUAL(PRESSED, pThis->state);
 }
 
 void test_keyboard_press_right_should_return_the_direction_seventy_seven(void){
-  movementShip *pThis = initiateState();
+  movementShip *pThis = initiateMovementState();
   
   pThis->state = RELEASE;
+  pThis->keyboard->buttonState = BUTTONHIT;
   getKbPressed_ExpectAndReturn(pThis->kbPressed, BUTTON_PRESSED);
   movementShipFSM(pThis);
   
-  TEST_ASSERT_EQUAL(KEY_RIGHT, pThis->direction);
+  TEST_ASSERT_EQUAL(KEY_RIGHT, pThis->keyboard->direction);
+  TEST_ASSERT_EQUAL(BUTTONHIT, pThis->keyboard->buttonState);
   TEST_ASSERT_EQUAL(PRESSED, pThis->state);
 }
 
 void test_keyboard_press_left_should_return_the_coordinateX_twenty_three(void){
-  movementShip *pThis = initiateState();
+  movementShip *pThis = initiateMovementState();
   
   pThis->coordinateX = 24;
   pThis->coordinateY = 47;
   pThis->state = PRESSED;
-  getKbCodeLeft_ExpectAndReturn(pThis->direction, KEY_LEFT);
+  pThis->keyboard->buttonState = BUTTONHIT;
+  getKbCodeLeft_ExpectAndReturn(pThis->keyboard->direction, KEY_LEFT);
   movementShipFSM(pThis);
   
   TEST_ASSERT_EQUAL(23, pThis->coordinateX);
@@ -67,13 +74,14 @@ void test_keyboard_press_left_should_return_the_coordinateX_twenty_three(void){
 }
 
 void test_keyboard_press_right_should_return_the_coordinateX_twenty_five(void){
-  movementShip *pThis = initiateState();
+  movementShip *pThis = initiateMovementState();
   
   pThis->coordinateX = 24;
   pThis->coordinateY = 47;
   pThis->state = PRESSED;
-  getKbCodeLeft_ExpectAndReturn(pThis->direction, KEY_RIGHT);
-  getKbCodeRight_ExpectAndReturn(pThis->direction, KEY_RIGHT);
+  pThis->keyboard->buttonState = BUTTONHIT;
+  getKbCodeLeft_ExpectAndReturn(pThis->keyboard->direction, KEY_RIGHT);
+  getKbCodeRight_ExpectAndReturn(pThis->keyboard->direction, KEY_RIGHT);
   movementShipFSM(pThis);
   
   TEST_ASSERT_EQUAL(25, pThis->coordinateX);
@@ -81,4 +89,5 @@ void test_keyboard_press_right_should_return_the_coordinateX_twenty_five(void){
   TEST_ASSERT_EQUAL(RELEASE, pThis->state);
 }
 
-
+ 
+ 
